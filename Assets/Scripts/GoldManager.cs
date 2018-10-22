d9d52459bcbd100e66633dabdb5782039724b6bc
Benjamin Armour <barmour@mail.pima.edu>
2018-10-22 12:06:35 -0700

    Menu and singleton created

---
+++ Assets/Scripts/GoldManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldManager : MonoBehaviour {
+
+	public static GoldManager singleton;
+	public int Gold;
+
+	// Use this for initialization
+	void Start () {
+		if(singleton != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            singleton = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LoadScenePlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenePlay : MonoBehaviour {
+
+	public string LevelToLoad;
+
+	public void loadthis(){
+		SceneManager.LoadScene(LevelToLoad);
+	}
+}
