df88104451a27b2bc0d92696012c95fc7a4ea504
Benjamin Armour <barmour@mail.pima.edu>
2018-10-25 19:25:37 -0700

    Inventory and craft system Almost complete
    Thanks drew!!!

---
+++ Assets/Scripts/WeaponInventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInventory : MonoBehaviour {
+
+    public List<Weapon> weapons = new List<Weapon>();
+
+    public void AddWeapon( Weapon newWeapon )
+    {
+        weapons.Add(newWeapon);
+    }
+}
