79e6bdcbdce1613adba6de2a49f48a46134867aa
Benjamin Armour <barmour@mail.pima.edu>
2018-11-17 11:36:33 -0700

    working on Weapon head creation and how to add to instantiated prefab.

---
+++ Assets/Scripts/Crafting/WeaponHead.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHead : MonoBehaviour {
+
+	public enum HeadType { WoodAxe, MetalAxe, MithralAxe, WoodHammer, MetalHammer, MithralHammer, WoodBlade, MetalBlade, MithralBlade }
+
+	public HeadType headType;
+
+	
+
+}
