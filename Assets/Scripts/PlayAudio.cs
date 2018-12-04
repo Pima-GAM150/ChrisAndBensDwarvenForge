ac4743f70bc49fcac44099d6dab31882a7c00047
Benjamin Armour <barmour@mail.pima.edu>
2018-12-03 17:53:24 -0700

    added the shadow and smith sound.

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+	public AudioSource SmithSound;
+
+	public void PlaySmithSound(){
+		SmithSound.Play(0);
+	}
+}
