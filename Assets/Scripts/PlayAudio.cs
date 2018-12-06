34d53d75bcfab80f25f94367d4d8c8cb9f689795
Benjamin Armour <barmour@mail.pima.edu>
2018-12-06 14:24:15 -0700

    Added walk sound to animation event triggers.

--- Assets/Scripts/PlayAudio.cs
+++ Assets/Scripts/PlayAudio.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PlayAudio : MonoBehaviour {
 
 	public AudioSource SmithSound;
+	public AudioSource StepSound;
 
 	public void PlaySmithSound(){
 		SmithSound.Play(0);
+	}
+
+	public void PlayStepSound(){
+		StepSound.Play(0);
+		print("Yo");
 	}
 }
